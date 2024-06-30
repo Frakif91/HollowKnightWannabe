16f7b5650203709d8f0a5b0a580794b392255d65
tyvgho <165305946+tyvgho@users.noreply.github.com>
2024-06-30 23:41:56 +0200

    machine volent
    j'ai rajouter le scripte de la machine volent pour quelle puis réstai adistence du joueur mes pas tros et que la machine volente puis tiré. le scripte n'est pas encore intégré au scripte général des énemit ducoup on peut ni fraper ni se prendre de dégât par cette dérgner

--- Godot/Nodes/Enemies/tir.gd
+++ Godot/Nodes/Enemies/tir.gd
@@ -3,11 +3,12 @@ var diférense = Vector2.ZERO
 var dont_mov = false
 func _on_vol_et_tir_fire(p):
 	dont_mov = false
-	position = p+diférense
+	position = p
 	diférense = PlayerStats.player.position-position 
 func _physics_process(delta):
 	if dont_mov == false :
 		velocity = diférense
 		move_and_slide()
 func _on_area_2d_body_entered(body):
-	pass
+	if body is MainCharacter:
+		position = Vector2(1000,1000)
