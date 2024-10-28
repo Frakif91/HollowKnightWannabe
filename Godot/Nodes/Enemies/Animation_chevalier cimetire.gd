e1dddb54ea5f3d64c2163d7b5c311993ae96ba24
tyvgho <165305946+tyvgho@users.noreply.github.com>
2024-10-28 13:48:43 +0100

    ad new sprite
    j'ai ajouter un sprite animer pour les plateforme mobile et j'ai changer les sprite du roach et ajouter des animation pour le joueur (attack en sautent )

--- Godot/Nodes/Enemies/Animation_chevalier cimetire.gd
+++ Godot/Nodes/Enemies/Animation_chevalier cimetire.gd
@@ -1,31 +1,39 @@
 extends CharacterBody2D
 var PlayerPosition = Vector2.ZERO
-var DchevalierPlayer = Vector2.ZERO
+var DchevalierPlayer = Vector2.ZERO #dinstance séparent le joueur et le bosse "chevailer"
 var vitese = Vector2(0.5,0)
-var is_animemete = false
+var a =false
+var is_animemete = false #est entrain de jouer une animation 
+
 @onready var animation_player = $sekelétoneconténeur/AnimationPlayer
 func _ready():
 	pass
 func _physics_process(delta):
+	if is_animemete == true :
+		mouvemen()
 	if is_animemete == false:
-		await mouvemen()
+		await animation()
 	PlayerPosition = PlayerStats.player.position
 	DchevalierPlayer = PlayerPosition - position
-	print(DchevalierPlayer)
+
+func animation():
+	if DchevalierPlayer[0] < -50 :
+		animation_player.play("marche")
+	elif DchevalierPlayer[0] > 50 :
+		animation_player.play("marche")
+	else :
+		animation_player.play("frap loude")
+	is_animemete= true
+
 
 func mouvemen():
 	if DchevalierPlayer[0] < -50 :
-		animation_player.play("marche")
 		position += -vitese
 	elif DchevalierPlayer[0] > 50 :
-		animation_player.play("marche")
 		position += vitese
-	else :
-		is_animemete = true
-		animation_player.play("frap loude")
-		await animation_player.animation_finished
-		is_animemete = false
+
+
 
 
 func _on_animation_player_animation_finished(anim_name):
-	pass # Replace with function body.
+	is_animemete = false
--- Godot/Scene/CharacterBody2D.gd
+++ Godot/Scene/CharacterBody2D.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 var toucher = false
 var premire_position = Vector2.ZERO
 @export var direction = Vector2.ZERO
+@onready var anim = $AnimatedSprite2D
 
-
+func _ready() -> void:
+	anim.play("stand")
 func _on_area_2d_body_entered(body):
 	if body is MainCharacter and toucher == false :
 		toucher = true
 		premire_position = position
 func _physics_process(delta):
 	if toucher == true:
+		anim.play("mouve")
 		velocity = direction
 		move_and_slide()
 		if velocity == Vector2.ZERO:
 			position = premire_position
 			toucher = false
+			anim.play("stand")
