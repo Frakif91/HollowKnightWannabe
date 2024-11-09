55bca20c956bbf94c09bd078be0f8f92c6d5f4ed
tyvgho <165305946+tyvgho@users.noreply.github.com>
2024-11-09 18:20:36 +0100

    ajout de Sprite + création d'un bak_groud
    j'ai mit une animation pour quant on détrui un mur dans laboratoire de bob plus remamis du de larireplant et des sprite des plateformemobile

--- Godot/Scene/mur désturctible.gd
+++ Godot/Scene/mur désturctible.gd
@@ -1,15 +1,17 @@
 extends StaticBody2D
 var proche = false
-
+@onready var anim = $"AnimatedSprite2D2"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Attack") and proche == true:
-		queue_free()
+		anim.play("default")
+
 
 func _body_entered(body):
 	if body is MainCharacter:
@@ -20,3 +22,8 @@ func _body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body is MainCharacter:
 		proche = false
+
+
+func _on_animated_sprite_2d_2_animation_finished() -> void:
+	print("a")
+	queue_free()
